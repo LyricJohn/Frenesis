f733ff2a65ce27948e619656a3e9cd73940be5ec
Lyric Johnson <165203031+LyricJohn@users.noreply.github.com>
2024-12-19 06:55:56 -0600

    Create magnets.jsx

---
+++ magnets/magnets.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const VELOCITY = 1000;
+
+const particles = [
+  { type: "proton", color: "red", MASS: 6 },
+  { type: "neutron", color: "blue", MASS: 6 },
+  { type: "electron", color: "blue", MASS: 1 },
+]
+
+const Particle = {( type, color, MULTIPLIER )} => {
+  
+}
