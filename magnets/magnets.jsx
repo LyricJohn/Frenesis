1d5c1300cf5ae11bdb66e1e827c81b2b0c9717de
Lyric Johnson <165203031+LyricJohn@users.noreply.github.com>
2024-12-19 07:12:00 -0600

    Added sphereGeometry to magnets.jsx

--- magnets/magnets.jsx
+++ magnets/magnets.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
 
-const VELOCITY = 1000;
+const TILE_SIZE = 1000;
+const WORLD_TILES = 10;
 
-const particles = [
-  { type: "proton", color: "red", MASS: 6 },
-  { type: "neutron", color: "blue", MASS: 6 },
-  { type: "electron", color: "blue", MASS: 1 },
-]
+const VELOCITY = 10;
 
-const Particle = {( type, color, MULTIPLIER )} => {
+/* const Particle = ({ type, color, MULTIPLIER }) => {
+  const attach = () {};
   
+  return (
+    <mesh>
+      <sphereGeometry args={[{mass}, {mass}, {mass}] />
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  );
+} */
+
+const App = () => {
+  const particles = [
+    { type: "proton", color: "red", MASS: 6 },
+    { type: "neutron", color: "blue", MASS: 6 },
+    { type: "electron", color: "blue", MASS: 1 },
+  ]
+
+  return
 }
